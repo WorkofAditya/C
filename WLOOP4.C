ea710fef5c20e10c4d6a4bc502c36f701411a67f
Adityasinh Sodha <lab.adityasinh@gmail.com>
2025-08-05 10:23:04 +0530

    Add files via upload

---
+++ WLOOP4.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+    // variable declarartion
+    // starting value (intial or intitialization)
+    int x=1,y=1;
+    clrscr();
+
+
+    while(y<=5)
+  {
+       x=1;
+
+       while(x<=5)
+  {
+      printf ("%d ",x);
+
+      x=x+1;
+  }
+
+    printf ("\n");
+    y=y+1;
+ }
+    getch();
+ }
+
