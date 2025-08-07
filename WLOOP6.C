a5e0e33af3b589927b59bde2cc38cf4945e8275d
Adityasinh Sodha <lab.adityasinh@gmail.com>
2025-08-07 10:45:07 +0530

    Add files via upload

---
+++ WLOOP6.C
@@ -0,0 +1,32 @@
+/*
+This program can print numbers with while loop
+Date: 7th Aug 2025
+Author: Adityasinh Sodha
+*/
+	 
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+    int x=1,y=1;
+    clrscr();
+
+
+    while(y<=5)
+  {
+       x=1;
+
+       while(x<=y)
+  {
+      printf ("%d ",x);
+
+      x=x+1;
+  }
+
+    printf ("\n");
+    y=y+1;
+ }
+    getch();
+ }
+
