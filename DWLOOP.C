aa93e9e4d7f1c0a6d6a4f2037d307f1bae478bae
Adityasinh Sodha <lab.adityasinh@gmail.com>
2025-08-11 10:46:28 +0530

    Added Comment

--- DWLOOP.C
+++ DWLOOP.C
@@ -1,5 +1,10 @@
-   #include <stdio.h>
-   #include <conio.h>
+/*
+This program can continuously perform multiplication until user enters 0, using Do while loop.
+Date: 11th Aug
+Author: Adityasinh Sodha
+*/
+#include <stdio.h>
+#include <conio.h>
    void main()
    {
 	int num=0, mul=1;
@@ -17,4 +22,5 @@
 	while(num!=0);
 	printf("\n multiplication=%d",mul);
 	getch();
+
    }
