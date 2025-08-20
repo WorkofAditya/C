7557992e6208052fb7d084d222f3afb23b23224c
Adityasinh Sodha <lab.adityasinh@gmail.com>
2025-08-20 10:08:26 +0530

    Add files via upload

---
+++ UDF1.C
@@ -0,0 +1,26 @@
+/*
+This program explains basic use of UDF
+Date: 20th Aug 2025
+Author: Adityasinh Sodha
+*/
+#include<stdio.h>
+#include<conio.h>
+void printdis();
+void main()
+{
+	clrscr();
+	printdis();
+	printf("\nfirst......");
+	printdis();
+	printf("\nsecond......");
+	printdis();
+	printf("\nthree......");
+	printdis();
+	getch();
+}
+void printdis()
+{
+	printf("\n++++++++++++++++++++++++");
+	printf("\n========================");
+	printf("\n########################");
+}
---
+++ UDF2.C
@@ -0,0 +1,30 @@
+/*
+This program perfrom arithnetic oprations using UDF
+Date: 20th Aug 2025
+Author: Adityasinh Sodha
+*/
+#include<stdio.h>
+#include<conio.h>
+void addi();
+void main()
+{
+	int x=0,y=0;
+	clrscr();
+	printf("enter two number:");
+	scanf("%d %d",&x,&y);
+	printf("\nsubtection : %d",x-y);
+	addi();
+	printf("\nmultipiction :%d",x*y);
+	addi();
+	getch();
+
+}
+void addi()
+{
+	int x=0,y=0,z=0;
+	printf("\nenter number :");
+	scanf("%d %d",&x,&y);
+	z=x+y;
+	printf("\nadditone : %d",z);
+
+}
