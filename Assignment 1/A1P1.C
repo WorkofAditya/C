17aec78ef66bbf9676b973cd850bc0e75f0755d2
Adityasinh Sodha <lab.adityasinh@gmail.com>
2025-09-03 10:24:38 +0530

    Update A1P1.C

--- Assignment 1/A1P1.C
+++ Assignment 1/A1P1.C
@@ -1,52 +1,22 @@
 /*
 This program can perfom arithmatic opration via user input
 Date: 23th july 2025
-Modifide: 26th july 2025
+Modified: 26th july 2025
 author: Adityasinh Sodha
 */
 #include<stdio.h>
 #include<conio.h>
-
 void main()
 {
-	int x, y, choice;
+	int x,y;
 	clrscr();
-	printf("input first digit: ");
+	printf("\nenter one number:");
 	scanf("%d",&x);
-	printf("input second digit: ");
+	printf("\nenter second number:");
 	scanf("%d",&y);
-
-	printf("\n1. Addition ");
-	printf("\n2. Substraction ");
-	printf("\n3. Multiplication ");
-	printf("\n4. division ");
-
-	printf("\n input your choice: ");
-	scanf("%d",&choice);
-
-
-	if(choice == 1)
-	{
-	printf("Addition = %d", x + y);
-	}
-
-	else if(choice == 2)
-	{
-	printf("\nSubstraction = %d", x - y);
-	}
-
-	else if(choice == 3)
-	{
-	printf("\nMultiplication = %d", x * y);
-	}
-
-	else
-	{
-	printf("\nDivision Quotient = %2f",(float)x/y);
-	printf("\nDivision Reminder = %d",x%y);
-	}
-
+	printf("\n1. Addition %d",x+y);
+	printf("\n2. Substraction %d",x-y);
+	printf("\n3. Multiplication %d",x*y);
+	printf("\n4. Division %.2f",(float)x/y);
 	getch();
-
-
 }
